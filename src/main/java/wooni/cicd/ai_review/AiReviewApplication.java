8a6cb6a4b41beda3549e1cb732a0868a61a5c77c
woo_ooni <heeey@nate.com>
2026-03-03 21:47:17 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/wooni/cicd/ai_review/AiReviewApplication.java
@@ -0,0 +1,13 @@
+package wooni.cicd.ai_review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wooni/cicd/ai_review/AiReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package wooni.cicd.ai_review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
