0068d44474899c6c0a40b6be68b45d5104a240cc
woo_ooni <heeey@nate.com>
2026-03-04 22:09:41 +0900

    test: GitHubApiService 테스트 추가 - 깃허브 연결 테스트 - PR 파일 조회 - 요약_리뷰_코멘트_등록

---
+++ src/test/java/wooni/cicd/ai_review/review/service/GitHubApiServiceTest.java
@@ -0,0 +1,35 @@
+package wooni.cicd.ai_review.review.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import wooni.cicd.ai_review.review.dto.DiffFile;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class GitHubApiServiceTest {
+    @Autowired
+    private GitHubApiService gitHubApiService;
+
+    @Test
+    @DisplayName("PR 파일 조회")
+    void PR_파일_조회_성공() {
+        List<DiffFile> files = gitHubApiService.getPrFiles("wooni0714/AI_CODE_REVIEW", 6)
+                .block();
+
+        System.out.println("파일 수: " + files.size());
+        files.forEach(f -> System.out.println("파일명: " + f.fileName()));
+    }
+
+    @Test
+    @DisplayName("요약 리뷰 코멘트 등록")
+    void 요약_리뷰_코멘트_등록_성공() {
+        gitHubApiService
+                .postSummaryComment("wooni0714/AI_CODE_REVIEW", 6, "테스트 요약 코멘트")
+                .block();
+    }
+}
