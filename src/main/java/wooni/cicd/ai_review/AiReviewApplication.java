7c32aa9dd8f1bfc42f6823ed919fc452ed874fa9
woo_ooni <heeey@nate.com>
2026-03-04 19:02:11 +0900

    feat: ConfigurationPropertiesScan 추가

--- src/main/java/wooni/cicd/ai_review/AiReviewApplication.java
+++ src/main/java/wooni/cicd/ai_review/AiReviewApplication.java
@@ -2,8 +2,10 @@ package wooni.cicd.ai_review;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class AiReviewApplication {
 
 	public static void main(String[] args) {
