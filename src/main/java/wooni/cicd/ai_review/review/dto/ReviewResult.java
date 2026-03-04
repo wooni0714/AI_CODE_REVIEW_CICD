cfd9d62882d3412aa84f462c585eaccb9ca8bd98
woo_ooni <heeey@nate.com>
2026-03-04 23:08:16 +0900

    fix: ReviewResult JSON 필드명 매핑 오류 수정

--- src/main/java/wooni/cicd/ai_review/review/dto/ReviewResult.java
+++ src/main/java/wooni/cicd/ai_review/review/dto/ReviewResult.java
@@ -1,11 +1,13 @@
 package wooni.cicd.ai_review.review.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record ReviewResult(
+        @JsonProperty("reviews")
         List<ReviewItem> reviewItems,
         String summary
 ) {
