15c1d6b460df772c66042f76b533efa6d12edad7
woo_ooni <heeey@nate.com>
2026-04-13 15:47:52 +0900

    test: PR 코드 리뷰 작동 테스트

--- src/main/java/wooni/cicd/ai_review/review_test/AiReviewTestService.java
+++ src/main/java/wooni/cicd/ai_review/review_test/AiReviewTestService.java
@@ -1,9 +1,11 @@
 package wooni.cicd.ai_review.review_test;
 
 import lombok.RequiredArgsConstructor;
+
 import org.apache.catalina.User;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -18,5 +20,18 @@ public class AiReviewTestService {
         for (String item : items) {
             System.out.println("saved: " + item);
         }
+    }
+
+    public List<String> getUserEmails(List<String> userIds) {
+        List<String> emails = new ArrayList<>();
+        for (String id : userIds) {
+            User user = findById(id);
+            emails.add(user.getUsername());
+        }
+        return emails;
+    }
+
+    private User findById(String id) {
+        return null;
     }
 }
